8e17b95cc1b0fb65f5bf210a5f6b129792cfddf0
Bivek <bivek.joshi@mercatus.com>
2019-10-04 11:15:36 -0400

    Updates

---
+++ precache-manifest.977dcfb00d24c09f4bb4a38ec1bf21d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000cde3bb3bb2401daf9689d9b0573dc",
+    "url": "/Basic-Recipe-Search-using-3rd-party-API/index.html"
+  },
+  {
+    "revision": "2e284eaa6e61359be71f",
+    "url": "/Basic-Recipe-Search-using-3rd-party-API/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "3c4262c758dcd4629ed4",
+    "url": "/Basic-Recipe-Search-using-3rd-party-API/static/js/2.14f00c49.chunk.js"
+  },
+  {
+    "revision": "2e284eaa6e61359be71f",
+    "url": "/Basic-Recipe-Search-using-3rd-party-API/static/js/main.7b8be73c.chunk.js"
+  },
+  {
+    "revision": "990d0e08a035b3abd364",
+    "url": "/Basic-Recipe-Search-using-3rd-party-API/static/js/runtime-main.91fd0733.js"
+  }
+]);
